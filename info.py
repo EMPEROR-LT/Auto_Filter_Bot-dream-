003c76ad3814714b9aedd0e1a91a6107efcac437
God Emperor <tarakindrajit@gmail.com>
2026-05-06 19:27:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -110,8 +110,8 @@ TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/SUPREME_BOTz")   # Third tu
 SHORTENER_API = environ.get("SHORTENER_API", "a032057e9b47f40a3c7d32a8d25e8bc4367bce7a") # Shortener API key
 SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "gplinks.com") # Shortener website
 
-SHORTENER_API2 = environ.get("SHORTENER_API2", "a7ac9b3012c67d7491414cf272d82593c75f6cbb")  # Shortener API key for second website
-SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "omegalinks.in") # Shortener website for second website
+SHORTENER_API2 = environ.get("SHORTENER_API2", "a032057e9b47f40a3c7d32a8d25e8bc4367bce7a")  # Shortener API key for second website
+SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "gplinks.com") # Shortener website for second website
 
 SHORTENER_API3 = environ.get("SHORTENER_API3", "a7ac9b3012c67d7491414cf272d82593c75f6cbb")  
 SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", "omegalinks.in") # Shortener website for third website

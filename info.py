bdbe542066ffe4e54655099a46aac1cba3311e11
God Emperor <tarakindrajit@gmail.com>
2026-05-28 14:02:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -75,7 +75,7 @@ STAR_PREMIUM_PLANS = {
 # ============================
 # MongoDB Configuration
 # ============================
-DATABASE_URI = environ.get('DATABASE_URI', "")  # MongoDB URI for the database
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://ltarak570_db_user:LGrlqUECbLwnRKMX@cluster0.wk9azn6.mongodb.net/?appName=Cluster0")  # MongoDB URI for the database
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0") # Database name (default: cluster)
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'dreamcinezone_files') # Collection name (default: dreamcinezone_files)
 
